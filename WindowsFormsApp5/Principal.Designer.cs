5322c87207a62f119d41e4b1ce88633dae75ca5b
Gordinho <gabrielsilvio201@gmail.com>
2018-08-04 16:48:36 -0300

    backup

--- WindowsFormsApp5/Principal.Designer.cs
+++ WindowsFormsApp5/Principal.Designer.cs
@@ -29,32 +29,32 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnUser = new System.Windows.Forms.Button();
-            this.BtnEstoque = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.LblUser = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnEstoque = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.btnUser);
             this.panel1.Controls.Add(this.BtnEstoque);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnUser);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
@@ -62,88 +62,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(152, 708);
             this.panel1.TabIndex = 0;
-            // 
-            // button3
-            // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
-            this.button3.Image = global::WindowsFormsApp5.Properties.Resources.update32;
-            this.button3.Location = new System.Drawing.Point(0, 581);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 127);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Transações";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
-            this.button2.Image = global::WindowsFormsApp5.Properties.Resources.update32;
-            this.button2.Location = new System.Drawing.Point(0, 454);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 127);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Transações";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
-            this.button1.Image = global::WindowsFormsApp5.Properties.Resources.update32;
-            this.button1.Location = new System.Drawing.Point(0, 327);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 127);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Transações";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // btnUser
-            // 
-            this.btnUser.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUser.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
-            this.btnUser.Image = global::WindowsFormsApp5.Properties.Resources.user32;
-            this.btnUser.Location = new System.Drawing.Point(0, 200);
-            this.btnUser.Name = "btnUser";
-            this.btnUser.Size = new System.Drawing.Size(152, 127);
-            this.btnUser.TabIndex = 4;
-            this.btnUser.Text = "Usuarios";
-            this.btnUser.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnUser.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnUser.UseVisualStyleBackColor = true;
-            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
-            // 
-            // BtnEstoque
-            // 
-            this.BtnEstoque.Dock = System.Windows.Forms.DockStyle.Top;
-            this.BtnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnEstoque.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnEstoque.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
-            this.BtnEstoque.Image = global::WindowsFormsApp5.Properties.Resources.box32;
-            this.BtnEstoque.Location = new System.Drawing.Point(0, 85);
-            this.BtnEstoque.Name = "BtnEstoque";
-            this.BtnEstoque.Size = new System.Drawing.Size(152, 115);
-            this.BtnEstoque.TabIndex = 3;
-            this.BtnEstoque.Text = "Estoque";
-            this.BtnEstoque.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.BtnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.BtnEstoque.UseVisualStyleBackColor = true;
-            this.BtnEstoque.Click += new System.EventHandler(this.BtnEstoque_Click);
             // 
             // panel3
             // 
@@ -179,17 +97,6 @@
             this.label2.Size = new System.Drawing.Size(71, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "Duck ";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox2.Image = global::WindowsFormsApp5.Properties.Resources.Mesa_de_trabajo_1;
-            this.pictureBox2.Location = new System.Drawing.Point(0, 4);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(85, 79);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
             // 
             // LblUser
             // 
@@ -245,6 +152,98 @@
             this.label4.TabIndex = 1001;
             this.label4.Text = "Logado como:";
             // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
+            this.button2.Image = global::WindowsFormsApp5.Properties.Resources.waiter;
+            this.button2.Location = new System.Drawing.Point(0, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 127);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Fornecedores";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnUser
+            // 
+            this.btnUser.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUser.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
+            this.btnUser.Image = global::WindowsFormsApp5.Properties.Resources.user32;
+            this.btnUser.Location = new System.Drawing.Point(0, 85);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(152, 127);
+            this.btnUser.TabIndex = 4;
+            this.btnUser.Text = "Usuarios";
+            this.btnUser.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUser.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnUser.UseVisualStyleBackColor = true;
+            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = global::WindowsFormsApp5.Properties.Resources.Mesa_de_trabajo_1;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(85, 79);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // BtnEstoque
+            // 
+            this.BtnEstoque.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEstoque.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEstoque.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
+            this.BtnEstoque.Image = global::WindowsFormsApp5.Properties.Resources.box32;
+            this.BtnEstoque.Location = new System.Drawing.Point(0, 339);
+            this.BtnEstoque.Name = "BtnEstoque";
+            this.BtnEstoque.Size = new System.Drawing.Size(152, 115);
+            this.BtnEstoque.TabIndex = 10;
+            this.BtnEstoque.Text = "Estoque";
+            this.BtnEstoque.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BtnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BtnEstoque.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
+            this.button1.Image = global::WindowsFormsApp5.Properties.Resources.update32;
+            this.button1.Location = new System.Drawing.Point(0, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 127);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Transações";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(132)))));
+            this.button3.Image = global::WindowsFormsApp5.Properties.Resources.update32;
+            this.button3.Location = new System.Drawing.Point(0, 581);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 127);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Transações";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // FrmPri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -262,9 +261,9 @@
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -277,13 +276,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnUser;
-        private System.Windows.Forms.Button BtnEstoque;
         private System.Windows.Forms.Label LblUser;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnEstoque;
     }
 }
